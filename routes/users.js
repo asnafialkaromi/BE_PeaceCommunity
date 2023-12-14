9ce55cdfdce8ff0406a70ea6755733008efb1c2b
[asnafialkaromi] <[asnafialkaromi4@gmail.com]>
2023-12-14 20:43:54 +0700

    update router get users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const routes = express();
 const { getUser, deleteUser } = require("../controllers/user");
+const router = express.Router();
 
 routes.get("/users", getUser);
 routes.delete("/user/:uuid", deleteUser);
 
-module.exports = routes;
+router.get("/users", (req, res) => {
+  // Set custom headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  // Your route logic goes here
+  getUser(req, res);
+});
+
+module.exports = router;
